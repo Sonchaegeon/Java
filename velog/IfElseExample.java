14dcbc1ac9b4c03980d111f26bed7a457f2a3c07
Sonchaegeon <thscorjs1002@naver.com>
2020-11-12 11:04:27 +0900

    Create IfElseExample.java

---
+++ velog/IfElseExample.java
@@ -0,0 +1,11 @@
+
+public class IfElseExample {
+	public static void main(String[] args) {
+		int score = 80;
+		if(score >= 90) {
+			System.out.println("A");
+		} else {
+			System.out.println("점수가 90보다 작습니다...");
+		}
+	}
+}
