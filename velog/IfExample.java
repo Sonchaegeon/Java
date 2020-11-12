855054c5c458862fb16112f8d54091cd7d62c361
Sonchaegeon <thscorjs1002@naver.com>
2020-11-12 11:04:04 +0900

    Create IfExample.java

---
+++ velog/IfExample.java
@@ -0,0 +1,14 @@
+
+public class IfExample {
+	public static void main(String[] args) {
+		int a = 15, b = 20;
+		
+		if(a > b) {
+			System.out.println("a가 b보다 큽니다!");
+		}
+		
+		if(b > a) {
+			System.out.println("b가 a보다 큽니다!");
+		}
+	}
+}
