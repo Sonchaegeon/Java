cd183575be727a1eea75d4477a47fb2bcf042b7e
sonchaegeon <thscorjs1002@naver.com>
2021-05-25 16:18:28 +0900

    Generic method

---
+++ Study/method_generic/Box.java
@@ -0,0 +1,8 @@
+package method_generic;
+
+public class Box<T> {
+
+    private T t;
+    public T getT() { return t; }
+    public void setT(T t) { this.t = t; }
+}
---
+++ Study/method_generic/Util.java
@@ -0,0 +1,16 @@
+package method_generic;
+
+public class Util {
+
+    public static <T> Box<T> boxing(T t) {
+        Box<T> box = new Box<T>();
+        box.setT(t);
+        return box;
+    }
+
+    public static <K, V> boolean compare(Pair<K, V> p1, Pair<K, V> p2) {
+        boolean keyCompare = p1.getKey().equals(p2.getKey());
+        boolean valueCompare = p2.getValue().equals(p2.getValue());
+        return keyCompare && valueCompare;
+    }
+}
